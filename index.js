82f25dda2b157157d9977e0dd3438f17e9a68771
CoderTofu <vioneverforget@gmail.com>
2024-08-05 22:50:02 +0800

    redeploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import cardRoutes from "./routes/cardRoutes.js";
 
 const app = express();
@@ -10,3 +9,16 @@ app.use(express.json());
 
 // Use like so: https:localhost:5000/api/(routes)
 app.use("/api", cardRoutes);
+
+// Route handler for the root URL
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+const PORT = 5000; // Specify the port number
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
